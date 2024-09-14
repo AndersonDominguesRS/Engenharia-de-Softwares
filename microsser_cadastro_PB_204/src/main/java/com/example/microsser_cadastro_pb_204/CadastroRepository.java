fde54f09997f383934ba38ae516481695dc878eb
AndersonDominguesRS <115754385+AndersonDominguesRS@users.noreply.github.com>
2024-09-13 22:16:34 -0300

    Delete microsser_cadastro_PB_204 directory

--- microsser_cadastro_PB_204/src/main/java/com/example/microsser_cadastro_pb_204/CadastroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.microsser_cadastro_pb_204;
-
-import com.example.microsser_cadastro_pb_204.model.Cadastro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CadastroRepository extends JpaRepository<Cadastro, Integer> {
-}
--- microsser_cadastro_PB_204/src/main/java/com/example/microsser_cadastro_pb_204/exception/ResourseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.microsser_cadastro_pb_204.exception;
-
-public class ResourseNotFoundException extends RuntimeException {
-    public ResourseNotFoundException(String mensagem) {
-        super(mensagem);
-    }
-}
