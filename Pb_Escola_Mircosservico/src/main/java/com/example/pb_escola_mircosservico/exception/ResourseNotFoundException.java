dc14d03aab883cfc078b350df54b14a18778335c
AndersonDominguesRS <115754385+AndersonDominguesRS@users.noreply.github.com>
2024-09-13 22:16:55 -0300

    Delete Pb_Escola_Mircosservico directory

--- Pb_Escola_Mircosservico/src/main/java/com/example/pb_escola_mircosservico/exception/ResourseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.pb_escola_mircosservico.exception;
-
-public class ResourseNotFoundException extends RuntimeException {
-    public ResourseNotFoundException(String mensagem) {
-        super(mensagem);
-    }
-}
